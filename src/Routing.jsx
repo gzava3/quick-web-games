43fe28b389eea3ede74f3a3a938b7d419bd01a57
Linda Yan <yanlinda690@gmail.com>
2024-04-26 17:05:20 -0500

    Game Ultimate Tic Tac Toe

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/Welcomepage";
 import Homepage from "./pages/Homepage";
+import UltimateTicTacToe from "./pages/UltimateTicTacToe";
 
 export default function Routing() {
   return (
     <Routes>
             <Route path="/" element={<WelcomePage />} />
             <Route path="/homepage" element={<Homepage />} />
+	    <Route path="/UltimateTicTacToe" element={<UltimateTicTacToe />} />
     </Routes>
   );
 }
